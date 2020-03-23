32693ac0d5f4538652c66cb9291e4e5b57c8ed99
Collin-Thornton <collin.thornton@okstate.edu>
2020-03-22 23:52:07 -0500

    l

---
+++ pa3/3.3_double_hash_table/include/hash_table.hpp
@@ -0,0 +1,31 @@
+#ifndef HASH_TABLE
+#define HASH_TABLE
+
+#include <string>
+
+class HashTable {
+    private:
+        struct Bucket {
+            bool full;
+            int key;
+        };
+
+        Bucket* table;
+        int s, length;
+
+    public:
+        HashTable(int length);
+        ~HashTable();
+
+        int insert(int key);
+        int remove(int key);
+
+        bool sch(int key);
+        int size();
+
+        std::string toString();
+
+};
+
+
+#endif
