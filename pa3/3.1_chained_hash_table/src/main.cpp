a9ab5b9f80287184fe6efeb7f8f8648ba1e89d9c
Collin-Thornton <collinthornton8@gmail.com>
2020-03-22 22:55:36 -0500

    sdfjk;

--- pa3/3.1_chained_hash_table/src/main.cpp
+++ pa3/3.1_chained_hash_table/src/main.cpp
@@ -33,21 +33,64 @@ const string HEADER = R"(
 using namespace std;
 
 int main(int argc, char** argv) {
-    HashTable table(13);
 
-    table.insert(3);
+    cout << HEADER << endl << endl << endl;
 
-    for(int i=0; i<15; ++i) {
-	    cout << "Operation: " << i << ".in\t" << endl;
-	    cout << "Table before operation:" << endl;
-	    cout << table.toString();
-	    int result = table.insert(i);
-	    if(result == -1) cout << "DUPLICATE RESULT" << endl;
-	    cout << "Table after operation:" << endl;
-	    cout << table.toString();
-    	    cout << endl << endl;
-    }
-    cout << "Operation: del(13)\t" << endl;
-    int result = table.remove(13);
-    cout << table.toString();
+    if(argc > 1) cout << "There are no command line arguments" << endl;
+
+    string fname;
+    cout << "Please specify the name of the file containing operations" << endl;
+    cout << "Filename: ";
+    cin >> fname;
+
+    //fname = "../test/test1.txt";
+    ifstream file(fname);
+    if(!file.is_open()) throw invalid_argument("FILE NOT FOUND");
+
+    string line;
+    getline(file, line, ' ');
+
+    short size;
+	int index = line.find(' ');
+	string sub = line.substr(0, index);
+	try {
+		size =stoi(sub);
+	} catch(exception e) {
+		cout << "Please inter an integer for size." << endl << "Exiting." << endl;
+		exit(0);
+	}
+
+	HashTable table = HashTable(size);
+
+	int prevIndex = index;
+	while(index != line.length()) {
+		string old_table = table.toString();
+
+		int index= line.find(' ', prevIndex);
+		string sub = line.substr(prevIndex, index-prevIndex);
+
+		string k = sub.substr(0, sub.find_first_of('.'));
+		int key;
+		try {
+			key = stoi(k);
+		} catch (exception e) {
+			cout << "Please enter an integer for key." << endl << "Exiting." << endl;
+			exit(0);
+		}
+
+		int result;
+		string cmd = sub.substr(sub.find_first_of('.'));
+		if(cmd == "del") result = table.remove(key);
+		else if(cmd == "in") result = table.insert(key);
+		else {
+			cout << "Command: " << sub << " unrecognized." << endl << "Exiting." << endl;
+			exit(0);
+		}
+
+		cout << "Operation:\t"  << sub << endl;
+        cout << "Old Table:\t"   << old_table << endl;
+        cout << "New Heap:\t"   << table.toString() << endl << endl;
+
+	}
+
 }
